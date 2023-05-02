45711ab5bfe9fbea076e0ba0e9ff9b5fc8868dfe
David <103842788+dagmendez@users.noreply.github.com>
2023-05-02 12:01:05 +0200

    Refactor Runner package

--- 01-core-contracts/src/main/scala/com/github/dagmendez/runner/AbstractCollectResultsRunner.scala
+++ 01-core-contracts/src/main/scala/com/github/dagmendez/runner/AbstractCollectResultsRunner.scala
@@ -2,11 +2,13 @@ package com.github.dagmendez.runner
 
 import com.github.dagmendez.command.Command
 
-trait AbstractCollectResultsRunner[Input, Output](results: Vector[Output]) extends Runner[Input, Output]:
+trait AbstractCollectResultsRunner[Input, Output] extends Runner[Input, Output]:
+  val internal_results: Vector[Output]
+
   def addResult(result: Output): AbstractCollectResultsRunner[Input, Output]
   def addAllResults(moreResults: Vector[Output]): AbstractCollectResultsRunner[Input, Output]
   def removeResult(result: Output): AbstractCollectResultsRunner[Input, Output]
 
 trait ConcurrentRunner[Input, Output] extends AbstractCollectResultsRunner[Input, Output]
 
-trait ConsecutiveRunner[Input, Output](results: Vector[Output]) extends AbstractCollectResultsRunner[Input, Output]
+trait ConsecutiveRunner[Input, Output] extends AbstractCollectResultsRunner[Input, Output]
--- 01-core-contracts/src/main/scala/com/github/dagmendez/runner/decorator/AbstractRunnerDecorator.scala
+++ 01-core-contracts/src/main/scala/com/github/dagmendez/runner/decorator/AbstractRunnerDecorator.scala
@@ -2,4 +2,7 @@ package com.github.dagmendez.runner.decorator
 
 import com.github.dagmendez.runner.Runner
 
-trait AbstractRunnerDecorator[Input, Output](runner: Runner[Input, Output]) extends Runner[Input, Output]
+trait AbstractRunnerDecorator[Input, Output] extends Runner[Input, Output]:
+  val runner: Runner[Input, Output]
+end AbstractRunnerDecorator
+
