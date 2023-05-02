4caad72815fa217be1ef85e2edbad0df90b63ae0
David <103842788+dagmendez@users.noreply.github.com>
2023-05-02 11:59:37 +0200

    Refactor Pipeline package

--- 01-core-contracts/src/main/scala/com/github/dagmendez/pipeline/Pipeline.scala
+++ 01-core-contracts/src/main/scala/com/github/dagmendez/pipeline/Pipeline.scala
@@ -7,25 +7,28 @@ import com.github.dagmendez.runner.Runner
 trait Pipeline[Input, Output]:
   def run(input: Input): Output
 
-trait CommandPipeline[Input, Output](commands: Vector[Command[Input, Output]]) extends Pipeline[Input, Output]:
+trait CommandPipeline[Input, Output] extends Pipeline[Input, Output]:
+  val commands: Vector[Command[Input, Output]]
   def addCommand(command: Command[Input, Output]): CommandPipeline[Input, Output]
+end CommandPipeline
 
-trait DiamondPipeline[Input, Output](runner: Runner[Input, Output], reconciler: Reconciler[Output])
-  extends CommandPipeline[Input, Output]
+trait DiamondPipeline[Input, Output] extends CommandPipeline[Input, Output]:
+  val runner: Runner[Input, Output]
+  val reconciler: Reconciler[Output]
+end DiamondPipeline
 
-trait ConcurrentPipeline[Input, Output](reconciler: Reconciler[Output]) extends DiamondPipeline[Input, Output]
+trait ConcurrentPipeline[Input, Output] extends DiamondPipeline[Input, Output]
 
 trait FlashPipeline[Input, Output] extends DiamondPipeline[Input, Output]
 
-trait ConsecutivePipeline[Input, Output](reconciler: Reconciler[Output]) extends DiamondPipeline[Input, Output]
+trait ConsecutivePipeline[Input, Output] extends DiamondPipeline[Input, Output]
 
 trait SimplePipeline[Input, Output] extends ConsecutivePipeline[Input, Output]
 
-trait TieredPipeline[Input, Output](
-  runner: Runner[Input, Output],
-  reconciler: Reconciler[Output],
-  tieredPipelines: Vector[TieredPipeline[Input, Output]]
-) extends DiamondPipeline[Input, Output]:
+trait TieredPipeline[Input, Output] extends DiamondPipeline[Input, Output]:
+  val tieredPipelines: Vector[TieredPipeline[Input, Output]]
+  
   def addTier(pipeline: Pipeline[Input, Output]): TieredPipeline[Input, Output]
+end TieredPipeline
 
 trait SimpleTieredPipeline[Input, Output] extends TieredPipeline[Input, Output]
