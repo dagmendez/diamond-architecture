a389b7b7e90d751bc6f78fd45277696dd8f8526f
David <103842788+dagmendez@users.noreply.github.com>
2023-06-02 23:48:39 +0200

    Fix typo

--- 01-core-contracts/src/main/scala/com/github/dagmendez/runner/Runner.scala
+++ 01-core-contracts/src/main/scala/com/github/dagmendez/runner/Runner.scala
@@ -2,4 +2,4 @@ package com.github.dagmendez.runner
 
 trait Runner[Input, Output]:
   def run(input: Input): Output
-  def addResult(convertedResult: Output): Outpu
+  def addResult(convertedResult: Output): Output
